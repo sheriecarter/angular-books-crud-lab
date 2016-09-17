ca74809bf1a60a026d22a0d66ea2cbcf82707f4d
Sherie Carter <sherie1.carter@gmail.com>
2016-09-16 18:14:18 -0700

    book input form

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ angular
 
    vm.newBook = {
        title: 'License to Ill',
-       authorName: 'Beastie Boys'
+       author: 'Beastie Boys',
+       image: 'https://cloud.githubusercontent.com/assets/7833470/10892116/865b1914-8156-11e5-967c-c139a8a052a5.jpg',
+       releaseDate: "April 1, 2002"
    };
+   vm.books =[
+     {
+       title: 'beyonce',
+       author: 'bey',
+       image: 'https://cloud.githubusercontent.com/assets/7833470/10892116/865b1914-8156-11e5-967c-c139a8a052a5.jpg',
+       releaseDate:"April 1, 2002"
+     },
+     {
+       title: 'rihanna',
+       author:'rihanna',
+       image:'https://cloud.githubusercontent.com/assets/7833470/10892116/865b1914-8156-11e5-967c-c139a8a052a5.jpg',
+       releaseDate:"April 1, 2002"
+     },
+     {
+       title:'jlo',
+       author:'jlo',
+       image:'https://cloud.githubusercontent.com/assets/7833470/10892116/865b1914-8156-11e5-967c-c139a8a052a5.jpg',
+       releaseDate:"April 1, 2002"
+     }
+
+   ];
  }
