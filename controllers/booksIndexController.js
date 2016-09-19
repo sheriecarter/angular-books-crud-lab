b8ebf4e5287e7c6855574df077acf1d4584685d1
Sherie Carter <sherie1.carter@gmail.com>
2016-09-19 06:05:00 -0700

    move controller for booksIndex to its own file

---
+++ controllers/booksIndexController.js
@@ -0,0 +1,24 @@
+
+
+
+
+ function booksIndexController () {
+  var vm = this;
+  vm.newBook = {};
+
+  vm.newBook = {
+      title: 'License to Ill',
+      author: 'Beastie Boys',
+      image: 'https://cloud.githubusercontent.com/assets/7833470/10892116/865b1914-8156-11e5-967c-c139a8a052a5.jpg',
+      releaseDate: "April 1, 2002"
+  };
+  // inside controller
+$http({
+  method: 'GET',
+  url: "https://super-crud.herokuapp.com/books"
+}).then(function successCallback(response) {
+ vm.books=response.data;
+ console.log(response);
+}, function errorCallback(response) {
+  console.log('There was an error getting the data', response);
+});
